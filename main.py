b170daf9d56051e5806eead7b7e0c1690bad1831
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-08-23 21:48:14 +0200

    respond keypress

--- main.py
+++ main.py
@@ -53,3 +53,8 @@ class AlienInvasion:
         self.ship.blitme()
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
